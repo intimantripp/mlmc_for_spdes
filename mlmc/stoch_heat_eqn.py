9493797cfe172ccc64f5242d89dddb108bbb00ec
Inti Mantripp <intimantripp@gmail.com>
2025-06-27 18:59:21 +0100

    Validated the results for SHE
    So I have got the analytic solution to integral u^2, and got my SHE MLMC converging to the true solution. The MC version converges faster, which is interesting / alarming. Next I want to add a further validation plot showing the results from actually performing the run.

--- mlmc/stoch_heat_eqn.py
+++ mlmc/stoch_heat_eqn.py
@@ -4,14 +4,13 @@ from mlmc.mlmc_test import mlmc_test
 
 def stoch_heat_eqn():
     nvert = 3
-    M = 8 
+    M = 8
     N = 1000
-    L = 6
-    N0 = 1000
+    L = 7
+    N0 = 100
     Eps = [0.005, 0.01, 0.02, 0.05, 0.1]
 
-    validation_value = 0.08527441178994156
-    validation_value = 4.138060043680517
+    validation_value = 1/12 - np.exp(- 2 * np.pi**2) / (2 * np.pi**2) # derivation of this can be seen in notebook 1_validation_work.ipynb
     mlmc_test(lambda l, N: stoch_heat_eqn_l(l, N), M, N, L, N0, Eps, nvert,validate=True, validation_value=validation_value)
     
 
@@ -32,23 +31,19 @@ def stoch_heat_eqn_l(l, N):
         dtc = lam * hc**2
         timesteps_c = nc**2
 
-    # std_f = np.sqrt(dtf / hf) # calculation outside the loop
-    std_f = np.sqrt(dtf) # REMOVE
+    std_f = np.sqrt(dtf / hf)
     sum1 = np.zeros(4)
     sum2 = np.zeros(2)
 
     for N1 in range(0, N, 100):
-        N2 = min(100, N-N1)
+        N2 = min(100, N - N1)
         uf = np.zeros((nf+1, N2))
 
         if l == 0:
             i = np.arange(1, nf) # indices of internal points, excluding the first and last
             for _ in range(timesteps_f):
-                # dWf = std_f * np.random.randn(nf - 1, N2) # different increments for each spatial point - noise
-                dWf = std_f * np.random.randn(N2)
-                # uf[i, :] += lam * (uf[i+1, :] - 2 * uf[i, :] + uf[i-1, :]) + dWf
-                uf[i, :] += lam * (uf[i+1, :] - 2 * uf[i, :] + uf[i-1, :]) + np.tile(dWf, (nf - 1, 1))
-            
+                dWf = std_f * np.random.randn(nf - 1, N2) # different increments for each spatial point - noise
+                uf[i, :] += lam * (uf[i+1, :] - 2 * uf[i, :] + uf[i-1, :]) + dWf
             Pf = hf * np.sum(uf**2, axis=0)
             Pc = np.zeros((1, N2))
         else:
@@ -57,19 +52,14 @@ def stoch_heat_eqn_l(l, N):
             i_c = np.arange(1, nc)
 
             for _ in range(timesteps_c):
-                # dWc = np.zeros((nc - 1, N2)) 
-                dWc = np.zeros(N2)
+                dWc = np.zeros((nc - 1, N2))
                 for _ in range(4): # 4 fine timesteps per coarse timestep
-                    # dWf = std_f * np.random.randn(nf - 1, N2)
-                    dWf = std_f * np.random.randn(N2)
-                    # uf[i_f, :] += lam * (uf[i_f+1, :] - 2 * uf[i_f, :] + uf[i_f-1, :]) + dWf
-                    uf[i_f, :] += lam * (uf[i_f+1, :] - 2 * uf[i_f, :] + uf[i_f-1, :]) + 10 * np.tile(dWf, (nf - 1, 1)) # REMOVE
-                    # dWc +=  dWf[:-1, :].reshape(nc-1, 2, N2).sum(axis=1) # sum of 2 fine adjacent increments, ignore final point
-                    dWc += dWf # REMOVE
-
-                # uc[i_c, :] += lam * (uc[i_c+1, :] - 2 * uc[i_c, :] + uc[i_c-1, :]) + dWc
-                uc[i_c, :] += lam * (uc[i_c+1, :] - 2 * uc[i_c, :] + uc[i_c-1, :]) + np.tile(dWc, (nc - 1, 1))
-            
+                    dWf = std_f * np.random.randn(nf - 1, N2)
+                    # dWf = std_f * np.random.randn(N2)
+                    uf[i_f, :] += lam * (uf[i_f+1, :] - 2 * uf[i_f, :] + uf[i_f-1, :]) + dWf
+                    dWc +=  dWf[:-1, :].reshape(nc-1, 2, N2).sum(axis=1) # sum of 2 fine adjacent increments, ignore final point
+                dWc *= 0.5
+                uc[i_c, :] += lam * (uc[i_c+1, :] - 2 * uc[i_c, :] + uc[i_c-1, :]) + dWc
             Pf = hf * np.sum(uf**2, axis=0)
             Pc = hc * np.sum(uc**2, axis=0)
 
--- mlmc/stoch_heat_eqn_qoi.py
+++ mlmc/stoch_heat_eqn_qoi.py
@@ -13,8 +13,8 @@ def default_qoi(u):
 def stoch_heat_eqn_qoi():
     nvert = 3
     M = 8
-    N = 10000
-    L = 5
+    N = 1000
+    L = 6
     N0 = 100
     Eps = [0.005, 0.01, 0.02, 0.05, 0.1]
     del1, del2, var1, var2 = mlmc_test(lambda l, N: stoch_heat_eqn_l(l, N), M, N, L, N0, Eps, nvert)
@@ -35,17 +35,20 @@ def stoch_heat_eqn_l(l, N, qoi_fn = default_qoi):
         hc = 1 / nc
         dtc = lam * hc**2
         timesteps_c = nc**2 # number of time steps for coarse grid with T = 0.25
+
+    std_f = np.sqrt(dtf / hf) # calculation outside the loop
     sum1 = np.zeros(4)
     sum2 = np.zeros(2)
-    std_f = np.sqrt(dtf / hf) # calculation outside the loop
+
     for N1 in range(0, N, 100):
-        N2 = min(100, N-N1)
+        N2 = min(100, N - N1)
         uf = np.zeros((nf+1, N2))
+
         if l == 0:
             i = np.arange(1, nf) # indices of internal points, excluding the first and last
             for _ in range(timesteps_f):
                 dWf = std_f * np.random.randn(nf - 1, N2) # different increments for each spatial point
-                uf[i, :] += lam * (uf[i+1, :] - 2 * uf[i, :] + uf[i-1, :]) + dWf
+                uf[i, :] += lam * (uf[i + 1, :] - 2 * uf[i, :] + uf[i-1, :]) + dWf
             
             # compute the quantity of interest for the fine grid
             Pf = qoi_fn(uf)
@@ -60,7 +63,7 @@ def stoch_heat_eqn_l(l, N, qoi_fn = default_qoi):
                 for _ in range(4):
                     dWf = std_f * np.random.randn(nf - 1, N2)
                     uf[i_f, :] += lam * (uf[i_f+1, :] - 2 * uf[i_f, :] + uf[i_f-1, :]) + dWf
-                    dWc +=  dWf[:-1, :].reshape(nc-1, 2, N2).sum(axis=1)  # sum of 2 fine adjacent increments, ignore final point
+                    dWc +=  dWf[:-1, :].reshape(nc-1, 2, N2).sum(axis=1) # sum of 2 fine adjacent increments, ignore final point
                     
                 # dWc is now the sum of 8 fine increments, which has the same variance as one coarse increment
                 uc[i_c, :] += lam * (uc[i_c+1, :] - 2 * uc[i_c, :] + uc[i_c-1, :]) + dWc
@@ -74,4 +77,5 @@ def stoch_heat_eqn_l(l, N, qoi_fn = default_qoi):
         sum1[3] += np.sum(diff**4)
         sum2[0] += np.sum(Pf)
         sum2[1] += np.sum(Pf**2)
+    
     return sum1, sum2 #sum1 is moments of MLMC estimator, sum2 is moments of MC estimator
